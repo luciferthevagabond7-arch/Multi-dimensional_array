978ebb91f1d13d9082197a58057d8a35ec2fc323
luciferthevagabond7-arch <luciferthevagabond7@gmail.com>
2025-11-17 20:30:49 +0530

    Update 2*2matrix.cpp

--- 2*2matrix.cpp
+++ 2*2matrix.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   int i,j,arr[2][2];
-  cout<<"Multidimensional array\n"<<endl<<
+  cout<<"Multidimensional array\n"<<endl;
   for(i=0;i<2;i++)
   {
     for(j=0;j<2;j++)
