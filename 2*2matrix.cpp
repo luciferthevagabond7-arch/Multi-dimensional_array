8d37a080e1efe32edc610d93a632694a5a6c9d8d
luciferthevagabond7-arch <luciferthevagabond7@gmail.com>
2025-11-17 20:21:10 +0530

    Create 2*2matrix.cpp
    2*2 matrix using multidimensional array

---
+++ 2*2matrix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,j,arr[2][2];
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      cout<<"Enter a ["<<i<<"]"<<"["<<j<<"]:";
+      cin>>arr[i][j];
+    }
+  }
+  for(i=0;i<2;i++)
+  {
+    cout<<"\n";
+    for(j=0;j<2;j++)
+    {
+      cout<<"\t"<<arr[i][j];
+    }
+  }
+  return 0;
+}
